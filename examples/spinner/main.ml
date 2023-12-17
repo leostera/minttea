b60c94338f8a2f49a960c93ac6d26c87612e37d9
Sabine Schmaltz <sabineschmaltz@gmail.com>
2023-12-17 18:45:57 +0100

    add spinners with Event.Frame example

--- examples/spinner/main.ml
+++ examples/spinner/main.ml
@@ -6,51 +6,51 @@ let mint fmt = Spices.(default |> fg (color "#77e5b7") |> build) fmt
 type spinner_state = {
   spinner : Spinner.spinner;
   frame : int;
-  ref : unit Riot.Ref.t;
+  last_frame : Ptime.t;
 }
-
-let set_timer (s : spinner_state) =
-  Command.Set_timer (s.ref, 1.0 /. float_of_int s.spinner.fps)
 
 type s = { spinners : spinner_state list }
 
 let initial_model =
+  let now = Ptime_clock.now () in
   {
     spinners =
       [
-        { spinner = Spinner.line; frame = 0; ref = Riot.Ref.make () };
-        { spinner = Spinner.dot; frame = 0; ref = Riot.Ref.make () };
-        { spinner = Spinner.mini_dot; frame = 0; ref = Riot.Ref.make () };
-        { spinner = Spinner.jump; frame = 0; ref = Riot.Ref.make () };
-        { spinner = Spinner.pulse; frame = 0; ref = Riot.Ref.make () };
-        { spinner = Spinner.points; frame = 0; ref = Riot.Ref.make () };
-        { spinner = Spinner.meter; frame = 0; ref = Riot.Ref.make () };
-        { spinner = Spinner.globe; frame = 0; ref = Riot.Ref.make () };
-        { spinner = Spinner.moon; frame = 0; ref = Riot.Ref.make () };
-        { spinner = Spinner.monkey; frame = 0; ref = Riot.Ref.make () };
-        { spinner = Spinner.hamburger; frame = 0; ref = Riot.Ref.make () };
-        { spinner = Spinner.ellipsis; frame = 0; ref = Riot.Ref.make () };
+        { spinner = Spinner.line; frame = 0; last_frame = now };
+        { spinner = Spinner.dot; frame = 0; last_frame = now };
+        { spinner = Spinner.mini_dot; frame = 0; last_frame = now };
+        { spinner = Spinner.jump; frame = 0; last_frame = now };
+        { spinner = Spinner.pulse; frame = 0; last_frame = now };
+        { spinner = Spinner.points; frame = 0; last_frame = now };
+        { spinner = Spinner.meter; frame = 0; last_frame = now };
+        { spinner = Spinner.globe; frame = 0; last_frame = now };
+        { spinner = Spinner.moon; frame = 0; last_frame = now };
+        { spinner = Spinner.monkey; frame = 0; last_frame = now };
+        { spinner = Spinner.hamburger; frame = 0; last_frame = now };
+        { spinner = Spinner.ellipsis; frame = 0; last_frame = now };
       ];
   }
 
-let init model = Command.Seq (List.map set_timer model.spinners)
+let init _ = Command.Noop
 
 let update event model =
   match event with
-  | Event.Timer ref ->
-      let update_spinner ~ref s =
-        if Riot.Ref.equal ref s.ref then
-          ( { s with frame = (s.frame + 1) mod Array.length s.spinner.frames },
-            Some (set_timer s) )
-        else (s, None)
+  | Event.Frame ->
+      let now = Ptime_clock.now () in
+      let update_spinner s =
+        let delta =
+          Ptime.diff now s.last_frame |> Ptime.Span.to_float_s |> Float.abs
+        in
+        if delta > 1.0 /. float_of_int s.spinner.fps then
+          {
+            s with
+            frame = (s.frame + 1) mod Array.length s.spinner.frames;
+            last_frame = now;
+          }
+        else s
       in
-      let spinners, cmds =
-        model.spinners |> List.map (update_spinner ~ref) |> List.split
-      in
-      ( { spinners },
-        cmds
-        |> List.find (fun (c : Minttea.Command.t option) -> Option.is_some c)
-        |> Option.value ~default:Command.Noop )
+      let spinners = model.spinners |> List.map update_spinner in
+      ({ spinners }, Command.Noop)
   | Event.KeyDown (Key "q") -> (model, Command.Quit)
   | _ -> (model, Command.Noop)
 
