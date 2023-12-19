44aeefdf4fa6afd0b29bba8ebd961aa585b2ed52
Leandro Ostera <leandro@abstractmachines.dev>
2023-12-19 13:08:29 +0100

    feat: better handle of escape sequences

--- minttea/event.ml
+++ minttea/event.ml
@@ -4,6 +4,6 @@ type t = KeyDown of string | Timer of unit Ref.t | Frame
 
 let pp fmt t =
   match t with
-  | KeyDown key -> Format.fprintf fmt "KeyDown(%s)" key
+  | KeyDown key -> Format.fprintf fmt "KeyDown(%S)" key
   | Timer ref -> Format.fprintf fmt "Timer(%a)" Ref.pp ref
   | Frame -> Format.fprintf fmt "Frame"
--- minttea/io_loop.ml
+++ minttea/io_loop.ml
@@ -4,13 +4,32 @@ open Tty
 
 type Message.t += Input of Event.t
 
-let translate = function " " -> "space" | key -> key
+let translate = function
+  | " " -> "<space>"
+  | "\027" -> "<esc>"
+  | "\027[A" -> "<up>"
+  | "\027[B" -> "<down>"
+  | "\027[C" -> "<left>"
+  | "\027[D" -> "<right>"
+  | "\127" -> "<backspace>"
+  | "\n" -> "<enter>"
+  | key -> key
 
 let rec loop runner =
   yield ();
   match Stdin.read_utf8 () with
   | `Read key ->
-      let msg = KeyDown (translate key) in
+      let msg =
+        match key with
+        | "\027" -> (
+            match Stdin.read_utf8 () with
+            | `Read "[" -> (
+                match Stdin.read_utf8 () with
+                | `Read key -> KeyDown (translate ("\027[" ^ key))
+                | _ -> KeyDown (translate key))
+            | _ -> KeyDown (translate key))
+        | key -> KeyDown (translate key)
+      in
       send runner (Input msg);
       loop runner
   | _ -> loop runner
