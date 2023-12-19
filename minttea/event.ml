fb4b23d218bae29a16947b1122b8a8ce4913fc2d
Leandro Ostera <leandro@abstractmachines.dev>
2023-12-19 13:14:51 +0100

    feat: better handle of escape sequences (#9)

--- minttea/event.ml
+++ minttea/event.ml
@@ -4,7 +4,7 @@ type t = KeyDown of string | Timer of unit Ref.t | Frame | Custom of Message.t
 
 let pp fmt t =
   match t with
-  | KeyDown key -> Format.fprintf fmt "KeyDown(%s)" key
+  | KeyDown key -> Format.fprintf fmt "KeyDown(%S)" key
   | Timer ref -> Format.fprintf fmt "Timer(%a)" Ref.pp ref
   | Frame -> Format.fprintf fmt "Frame"
   | Custom _msg -> Format.fprintf fmt "Custom"
