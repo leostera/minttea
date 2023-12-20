58857b875f9ae22520734d782b3d19cb5f173528
Leandro Ostera <leandro@abstractmachines.dev>
2023-12-20 01:07:45 +0100

    feat: move initial model outwards
    This change eases reusing apps with different initial states, which can
    be good to skip the current state of the application based on other
    inputs (like CLI arguments) or even for testing purposes (pass in any
    state you want to test).

--- minttea/minttea.ml
+++ minttea/minttea.ml
@@ -5,14 +5,14 @@ module App = App
 
 let app = App.make
 
-let start app =
+let start app ~initial_model =
   let prog = Program.make ~app ~fps:60 in
   let module App = struct
     let name = "my_app"
 
     let start () =
       Logger.set_log_level None;
-      let pid = spawn_link (fun () -> Program.run prog) in
+      let pid = spawn_link (fun () -> Program.run prog initial_model) in
       Ok pid
   end in
   Riot.start ~apps:[ (module Riot.Logger); (module App) ] ()
