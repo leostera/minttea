506b2583463e69436fa20c8e23a4378cafecce08
Michael J Erwin <michaeljameserwin@gmail.com>
2024-03-11 15:29:42 -0400

    Add nix flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,13 @@
   description = "A little TUI framework for OCaml";
 
   inputs.colors.url = "github:ocaml-tui/colors";
+  inputs.colors.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.minttea.url = "github:leostera/minttea";
+  inputs.minttea.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.riot.url = "github:riot-ml/riot";
+  inputs.riot.inputs.nixpkgs.follows = "nixpkgs";
   inputs.tty.url = "github:ocaml-tui/tty";
+  inputs.tty.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = inputs@{ flake-parts, ... }:
@@ -18,11 +24,36 @@
             devShells = {
               default = mkShell {
                 buildInputs = [ ocamlPackages.utop ];
-                inputsFrom = [ self'.packages.spices ];
+                inputsFrom = [ self'.packages.spices self'.packages.default self'.packages.leaves ];
               };
             };
 
             packages = {
+              default = buildDunePackage {
+                inherit version;
+                pname = "minttea";
+                propagatedBuildInputs = with ocamlPackages; [
+                  inputs'.riot.packages.default
+                  (mdx.override {
+                    inherit logs;
+                  })
+                  inputs'.tty.packages.default
+                  uuseg
+                ];
+                src = ./.;
+              };
+              leaves = buildDunePackage {
+                inherit version;
+                pname = "leaves";
+                propagatedBuildInputs = with ocamlPackages; [
+                  self'.packages.default
+                  (mdx.override {
+                    inherit logs;
+                  })
+                  inputs'.minttea.packages.spices
+                ];
+                src = ./.;
+              };
               spices = buildDunePackage {
                 inherit version;
                 pname = "spices";
